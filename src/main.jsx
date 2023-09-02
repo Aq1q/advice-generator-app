a073e1b6fff71822b57147ecb05f1b7f19ea2477
Michał Salabura <micheu334@gmail.com>
2023-09-02 15:59:29 +0200

    fix: icon import

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.scss'
-import './assets/favicon-32x32.png'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
