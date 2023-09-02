e0f45a6a373146c4d005eafde10f20c0540040af
Michał Salabura <micheu334@gmail.com>
2023-09-02 13:17:59 +0200

    feat: fetch advice

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import { useEffect, useState } from "react"
+
+
 function App() {
- 
+  const [advice, setAdvice] = useState(null)
+  useEffect(() => {
+    const fetchData = async () => {
+    const response = await fetch('https://api.adviceslip.com/advice');
+    const json = await response.json();
+    setAdvice(json);
+    };
+    fetchData();
+  }, []);
+  
   return (
     <>
-     
+     <div>{advice.slip.id}</div>    
+     <div>{advice.slip.advice}</div>
     </>
   )
 }
