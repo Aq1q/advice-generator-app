433d4a3269ca3cb7f9e767806e152b7969346f20
Michał Salabura <micheu334@gmail.com>
2023-09-02 13:30:49 +0200

    feat: fetch data using useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,40 @@ import { useEffect, useState } from "react"
 
 
 function App() {
-  const [advice, setAdvice] = useState(null)
+  const [advice, setAdvice] = useState({
+    slip: {
+      id: null,
+      advice: '',
+    }
+  });
+const [isLoading, setIsLoading] = useState(false);
+const [isError, setIsError] = useState(false);
+
   useEffect(() => {
     const fetchData = async () => {
-    const response = await fetch('https://api.adviceslip.com/advice');
-    const json = await response.json();
-    setAdvice(json);
+      setIsError(false);
+      setIsLoading(true);
+      try {
+        const response = await fetch('https://api.adviceslip.com/advice');
+        const json = await response.json();
+        setAdvice(json);
+      } catch {
+        setIsError(true);
+      }
+      setIsLoading(false);
     };
     fetchData();
   }, []);
   
   return (
     <>
-     <div>{advice.slip.id}</div>    
-     <div>{advice.slip.advice}</div>
+      {isError && <div>Someting went wrong...</div>}
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <><div>{advice.slip.id}</div><div>&quot;{advice.slip.advice}&quot;</div></>
+      )}
+      
     </>
   )
 }
